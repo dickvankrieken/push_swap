cc99850e783285273f76795d3f3e6b3921bdd5c4
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2021-08-22 16:39:34 +0200

    update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dvan-kri <dvan-kri@student.42.fr>            +#+                      #
 #                                                    +#+                       #
 #    Created: 2021/08/15 14:04:55 by dvan-kri      #+#    #+#                  #
-#    Updated: 2021/08/15 15:00:09 by dvan-kri      ########   odam.nl          #
+#    Updated: 2021/08/22 12:37:49 by dvan-kri      ########   odam.nl          #
 #                                                                              #
 #******************************************************************************#
 
@@ -18,12 +18,14 @@ SRC_PS = push_swap.c
 
 SRC_C = checker.c
 
+OBJS_PS = $(SRCS:.c=.o)
+
 LIBFT = libft/libft.a
 
 all: $(NAME_PS)
 
 $(NAME_PS):
-	gcc -o $(NAME_PS) $(LIBFT) $(SRC_PS)
+	gcc -o $(NAME_PS) $(LIBFT) $(OBJS_PS)
 
 $(NAME_C):
 	gcc -o $(NAME_C) $(SRC_C)
@@ -35,3 +37,5 @@ fclean: clean
 	/bin/rm -f $(NAME)
 
 re: fclean all
+
+.PHONY: all clean fclean re
--- push_swap.c
+++ push_swap.c
@@ -6,16 +6,31 @@
 /*   By: dvan-kri <dvan-kri@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/15 14:47:32 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2021/08/15 15:19:00 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2021/08/22 16:05:16 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/ft_printf.h"
 #include "push_swap.h"
 
+void	init_stack(t_stack **stack, char **argv)
+{
+	t_node	*new_node;
+
+
+}
+
+void	push_swap(int argc, char **argv)
+{
+	t_stack *stack_a;
+
+	init_stack(stack_a, argv);
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc < 2)
 		ft_printf("Not enough arguments");
-
+	else
+		push_swap(argc, argv);
 }
