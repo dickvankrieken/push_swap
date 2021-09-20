9747e3574be4cab5ab64104e32e095a776dbbc5c
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2021-09-20 14:34:51 +0200

    fix swap

--- srcs/swap.c
+++ srcs/swap.c
@@ -2,33 +2,19 @@
 
 void	swap(t_stack *stack)
 {
-	t_node	*tmp;
+	t_node	*first;
+	t_node	*second;
 
-	// if (stack->size > 1)
-	// {
-	// 	new_head = stack->head->next;
-	// 	stack->head->next = stack->head->next->next;
-	// 	stack->head->prev = new_head;
-	// 	new_head->next = stack->head;
-	// 	stack->head = new_head;
-	// 	stack->tail->next = new_head;
-	// 	if (stack->size == 2)
-	// 		stack->tail = new_head->next;
-	// }
+	first = stack->head;
+	second = stack->head->next;
 	if (stack->size < 2)
 		return ;
-	tmp = stack->head;
-	
+	if (stack->size == 2)
+		stack->tail = first;
+	first->prev = stack->head->next;
+	first->next = stack->head->next->next;
+	second->next = first;
+	second->prev = stack->tail;
+	stack->tail->next = second;
+	stack->head = second;
 }
-
-/*
-tmp wordt gelijk gesteld aan de head
-
-wat moet er veranderen:
-
-stack->head wordt stack->head->next
-stack->head->next wordt tmp
-stack->head->prev wordt 
-als de size == 2 verandert de tail naar tmp en de tmp->next wordt gelijkgesteld aan stack->head
-stack->head->prev wordt 
- */
